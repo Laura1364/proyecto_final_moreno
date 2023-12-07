f7928fbb9b98d34390aa93464cb102d704b78fad
laura1364 <ltumino@gmail.com>
2023-12-07 16:51:40 -0300

    modelos

--- project/bioapp/models.py
+++ project/bioapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class escuela (models.Models):
+    numero_escuela = models.Charfield(max_length=100, unique=True)
+
+class alumno (models.Models):
+    nombre = models.Charfield(max_length=100, unique=True)
+    apellido = models.Charfield(max_length=100, unique=True)
+    mail = models.EmailField(max_length=254)
+
+class materia (models.Models):
+    materia = models.Charfield(max_length=100, unique=True)
+    nota = models.DecimalField(max_digits=5, decimal_places=2)
