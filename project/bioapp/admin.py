b8f58030914b4128dbfc4b4d55e1cc2e43deccfb
laura1364 <ltumino@gmail.com>
2023-12-12 21:13:06 -0300

    migration

--- project/bioapp/admin.py
+++ project/bioapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.escuela)
+admin.site.register(models.alumno)
+admin.site.register(models.materia)
--- project/bioapp/models.py
+++ project/bioapp/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
-class escuela (models.Models):
-    numero_escuela = models.Charfield(max_length=100, unique=True)
+class escuela(models.Model):
+    escuela = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.escuela}"
+ 
 
-class alumno (models.Models):
-    nombre = models.Charfield(max_length=100, unique=True)
-    apellido = models.Charfield(max_length=100, unique=True)
+class alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
     mail = models.EmailField(max_length=254)
 
-class materia (models.Models):
-    materia = models.Charfield(max_length=100, unique=True)
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+class materia(models.Model):
+    materia = models.CharField(max_length=100)
     nota = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.materia} ({self.nota})"
