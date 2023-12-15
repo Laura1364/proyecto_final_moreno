577bf3b4c7cc46748a274a5276f76fa6abea9ab6
laura1364 <ltumino@gmail.com>
2023-12-15 09:38:22 -0300

    formularios

--- project/bioapp/forms.py
+++ project/bioapp/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class AlumnoFormulario(forms.Form):
     nombre = forms.CharField(max_length=100)
     apellido = forms.CharField(max_length=100)
-
+    mail = forms.EmailField(max_length=200)
 class AlumnoBuscarFormulario(forms.Form):
     apellido = forms.CharField(max_length=50)
     
--- project/bioapp/models.py
+++ project/bioapp/models.py
@@ -10,9 +10,11 @@ class Escuela (models.Model):
 class Alumno (models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    
+    mail = models.EmailField(max_length=200)
+
+
     def __str__(self):
-        return f"{self.nombre} {self.apellido}"
+        return f"{self.nombre} {self.apellido} {self.mail}"
     
     
 class Materia (models.Model):
